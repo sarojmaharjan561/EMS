da7e4c04740b64806f32a2da777c91576578dd8b
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2023-07-23 12:54:32 +1000

    added company seeder

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'website' => $this->faker->url(),
+            'created_at' => now(),
+        ];
+    }
+}
